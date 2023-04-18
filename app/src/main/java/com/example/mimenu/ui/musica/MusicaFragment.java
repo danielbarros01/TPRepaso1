f02b4a57c6530bdab7f5154a8ed1327d6dca93c2
danielbarros01 <danibelgranocab@hotmail.com>
2023-04-18 00:03:53 -0300

    Servicio musical agregado

--- app/src/main/java/com/example/mimenu/ui/musica/MusicaFragment.java
+++ app/src/main/java/com/example/mimenu/ui/musica/MusicaFragment.java
@@ -2,6 +2,9 @@ package com.example.mimenu.ui.musica;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,21 +14,91 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.mimenu.R;
 
 public class MusicaFragment extends Fragment {
 
     private MusicaViewModel mViewModel;
+    private ImageButton btnPlay, btnNext;
 
     public static MusicaFragment newInstance() {
         return new MusicaFragment();
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_musica, container, false);
+        View view = inflater.inflate(R.layout.fragment_musica, container, false);
+        btnPlay = view.findViewById(R.id.btnPlay);
+        btnNext = view.findViewById(R.id.btnNext);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                next();
+            }
+        });
+    }
+
+    public void pause() {
+        btnPlay.setImageResource(android.R.drawable.ic_media_play);
+
+        //Pausar la cancion
+        Intent intent = new Intent(getActivity(), ServicioMusical.class);
+        intent.setAction("PAUSE");
+        getActivity().startService(intent);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+            }
+        });
+    }
+
+    public void play() {
+        btnPlay.setImageResource(android.R.drawable.ic_media_pause);
+
+        //Darle que ande
+        Intent intent = new Intent(getActivity(), ServicioMusical.class);
+        intent.setAction("PLAY");
+
+        getActivity().startService(intent);
+
+
+        // Definir un nuevo OnClickListener para el botón
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+            }
+        });
+    }
+
+    public void next() {
+        //Siguiente cancion
+        Intent intent = new Intent(getActivity(), ServicioMusical.class);
+        intent.setAction("NEXT");
+        getActivity().startService(intent);
     }
 
     @Override
@@ -34,5 +107,4 @@ public class MusicaFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(MusicaViewModel.class);
         // TODO: Use the ViewModel
     }
-
 }
